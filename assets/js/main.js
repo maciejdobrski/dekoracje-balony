5400ac579247db06d47f141152c5904f8e38b15c
Maciej Dobrski <maciejdobrski@gmail.com>
2016-11-23 11:55:55 +0100

    Dodanie google-map

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ $(function () {
         $('html, body').animate({
             scrollTop: $(hash).offset().top -80
         }, 2000, function () {
-            
+
         });
 
         $('.navbar-collapse ul li').on("click", function(){
@@ -48,4 +48,16 @@ $(function () {
     function getCurrentScroll() {
         return window.pageYOffset || document.documentElement.scrollTop;
     }
+
+    var latitude = $('#google-map').data('latitude');
+    var longitude = $('#google-map').data('longitude');
+
+    function initialize_map() {
+        var myLatlng = new google.maps.LatLng(latitude, longitude);
+        var mapOptions = {zoom: 14, scrollwheel: false, center: myLatlng};
+        var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+        /*var marker = new google.maps.Marker({position: myLatlng, map: map});*/
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize_map);
 });
