d7fb90aabb0ea5aed04e310df10dad0515b2d947
Maciej Dobrski <maciejdobrski@gmail.com>
2016-11-23 12:43:14 +0100

    Przeniesienie google-map nad sekcje kontakt, drobne poprawki w js-ie, oraz stylowanie mapy

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,10 +54,29 @@ $(function () {
 
     function initialize_map() {
         var myLatlng = new google.maps.LatLng(latitude, longitude);
-        var mapOptions = {zoom: 14, scrollwheel: false, center: myLatlng};
+        var mapOptions = {
+            zoom: 15,
+            scrollwheel: false,
+            center: myLatlng,
+            streetViewControl: true,
+            scaleControl: true
+        };
+
         var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-        /*var marker = new google.maps.Marker({position: myLatlng, map: map});*/
+        //var marker = new google.maps.Marker({position: myLatlng, map: map});
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            draggable: false
+        });
+
+        google.maps.event.addListener(marker,'click',function() {
+            map.setZoom(16);
+            map.setCenter(marker.getPosition());
+        });
     }
 
     google.maps.event.addDomListener(window, 'load', initialize_map);
+
 });
